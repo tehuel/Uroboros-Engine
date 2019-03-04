cf0ff2d04a1cacfa6bfdda249b6207b74538e3fa
gguzman89 <ggtpi0gabriel@gmail.com>
2019-03-03 23:43:26 -0300

    mouse translate reference

--- src/main/java/org/team/uroboros/uroboros/engine/input/Mouse.java
+++ src/main/java/org/team/uroboros/uroboros/engine/input/Mouse.java
@@ -6,16 +6,18 @@ import java.awt.event.MouseMotionListener;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
 
+import org.team.uroboros.uroboros.engine.geometry.Point;
+
 public enum Mouse implements MouseMotionListener, MouseWheelListener, MouseListener  {
 	
-	BUTTON1, BUTTON2, BUTTON3, MOUSE_WHEEL, MOUSE_CLICKED, MOUSE_DRAGGED, MOUSE_PRESSED, MOUSE_RELEASED;
+	BUTTON1, BUTTON2, BUTTON3, MOUSE_WHEEL, MOUSE_CLICKED, MOUSE_DRAGGED, MOUSE_PRESSED, MOUSE_RELEASED, MOUSE_MOVED;
 	
-	private int keyCode;
 	private Boolean isPressed = false;
+	private Point lastPosition = Point.ORIGIN;
 	
-	private Mouse() {
-		// creo q es por aca!!!
-	}
+//	private Mouse() {
+//		// creo q es por aca!!!
+//	}
 
 	public Boolean isPressed() {
 		return isPressed;
@@ -23,34 +25,36 @@ public enum Mouse implements MouseMotionListener, MouseWheelListener, MouseListe
 	
 	@Override
 	public void mouseDragged(MouseEvent e) {
-		if(e.getButton() == MouseEvent.MOUSE_DRAGGED) {
-			isPressed = true;
-//			e.translatePoint(x, y);
-			// alcanza con saber que esta presionada?
-		}
-		
+		setLastPosition(new Point(e.getX(), e.getY()*(-1)));
 	}
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
-		// tiene que definir un nuevo punto?
+		 setLastPosition(new Point(e.getX() * 0.5, (e.getY() * 0.5) * -1));
+		 System.out.println(e.getX());
+//		 e.getPoint()
+		 /**
+		  * Transformacion del Mouse respecto de canvas
+		  * 
+		  */
+		 System.out.println(e.getY()*(-1));
 	}
 
 	@Override
 	public void mouseWheelMoved(MouseWheelEvent e) {
-		if(e.getWheelRotation() < 0 ) {
-			// negative values rotated up
-		} else {
-			// positive values rotated down
-		}
-		
+//		if(e.getWheelRotation() < 0 ) {
+//			// negative values rotated up
+//		} else {
+//			// positive values rotated down
+//		}
+//		
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		if(e.getButton() == MouseEvent.MOUSE_CLICKED) {
-			isPressed = true;
-		}
+//		if(e.getButton() == MouseEvent.MOUSE_CLICKED) {
+//			isPressed = true;
+//		}
 	}
 
 	@Override
@@ -67,16 +71,24 @@ public enum Mouse implements MouseMotionListener, MouseWheelListener, MouseListe
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		if(e.getButton() == MouseEvent.MOUSE_PRESSED) {
-			isPressed = true;
-		}
+//		if(e.getButton() == MouseEvent.MOUSE_PRESSED) {
+//			isPressed = true;
+//		}
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		if(e.getButton() == MouseEvent.MOUSE_RELEASED) {
-			isPressed = false;
-		}
+//		if(e.getButton() == MouseEvent.MOUSE_RELEASED) {
+//			isPressed = false;
+//		}
+	}
+
+	public Point getLastPosition() {
+		return lastPosition;
+	}
+
+	public void setLastPosition(Point lastPosition) {
+		this.lastPosition = lastPosition;
 	}
 
 }
